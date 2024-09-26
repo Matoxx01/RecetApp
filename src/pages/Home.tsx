22a15eeead70817a5bfce5650996b0e5607af2bf
Matoxx01 <matiasbarrios2010@gmail.com>
2024-09-25 21:12:50 -0300

    Ahora si

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,66 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import {
+  IonContent, 
+  IonHeader, 
+  IonPage, 
+  IonTitle,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonList,
+  IonCardContent,
+  IonLabel,
+  IonMenu,
+  IonItem,
+  IonButtons,
+  IonMenuButton,
+  IonToolbar
+ } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
-const Home: React.FC = () => {
+function Home() {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+    <>
+      <IonMenu contentId="main-content">
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle>Menú</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
+        <IonContent className="ion-padding">
+          <IonList>
+            <IonItem button>
+              <IonLabel>Iniciar sesión</IonLabel>
+            </IonItem>
+            <IonItem button>
+              <IonLabel>Configuración</IonLabel>
+            </IonItem>
+            <IonItem button>
+              <IonLabel>Sobre Nosotros</IonLabel>
+            </IonItem>
+          </IonList>
+        </IonContent>
+      </IonMenu>
+      <IonPage id="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot="start">
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>ResetApp</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <IonCard button={true} className="card-custom">
+            <img alt="Fetuccini Alfredo" src="Fetuccini Alfredo.webp" />
+            <IonCardHeader>
+              <IonCardTitle>Fetuccini Alfredo</IonCardTitle>
+            </IonCardHeader>
+              <IonCardContent>El clásico fetuccini alfredo cremosito, sencillo y muy rico. Añádele la proteína que más te guste.</IonCardContent>
+          </IonCard>
+        </IonContent>
+      </IonPage>
+    </>
   );
 };
 
