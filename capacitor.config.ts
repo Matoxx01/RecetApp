c77618bceedce55492f66c129e08eeecf7f7b56b
Matoxx01 <matiasbarrios2010@gmail.com>
2024-12-16 16:04:26 -0300

    Cambio de nombre en los archivos internos de la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'ResetApp3.0',
+  appName: 'RecetApp',
   webDir: 'dist'
 };
 
